7f10bfc62e941968b181a6744241c391aa1e4736
codetuner <codetuner@users.noreply.github.com>
2022-09-22 00:49:08 +0200

    Fixed routing and other small issues

--- MyMvcApp/Areas/MvcDashboardIdentity/Controllers/EntityExtensions.cs
+++ MyMvcApp/Areas/MvcDashboardIdentity/Controllers/EntityExtensions.cs
@@ -12,15 +12,15 @@ namespace MyMvcApp.Areas.MvcDashboardIdentity.Controllers
         /// Performs ordering given a string expression.
         /// </summary>
         /// <param name="query">The query to order.</param>
-        /// <param name="orderByExpression">The property (path) to order by. Append " ASC" or " DESC" to be explicit about the ordering direction.</param>
+        /// <param name="orderByExpression">The property (path) to order by. Append " ASC" or " DESC" to be explicit about the ordering direction.
+        /// Can contain multiple terms separated by comma, i.e. "Name, Town ASC, Country DESC".
+        /// </param>
         /// <see href="https://stackoverflow.com/a/64085775"/>
-        public static IQueryable<T> OrderBy<T>(this IQueryable<T> query, string orderByExpression)
+        public static IOrderedQueryable<T> OrderBy<T>(this IQueryable<T> query, string orderByExpression)
         {
-            if (string.IsNullOrEmpty(orderByExpression))
-                return query;
-
             string propertyName, orderByMethod;
-            string[] strs = orderByExpression.Split(' ');
+            string[] terms = orderByExpression.Split(',', 2);
+            string[] strs = terms[0].Split(' ', StringSplitOptions.RemoveEmptyEntries);
             propertyName = strs[0];
 
             if (strs.Length == 1)
@@ -34,7 +34,51 @@ namespace MyMvcApp.Areas.MvcDashboardIdentity.Controllers
             MethodCallExpression orderByCall = Expression.Call(typeof(Queryable), orderByMethod, new Type[] { query.ElementType, me.Type }, query.Expression
                 , Expression.Quote(Expression.Lambda(me, pe)));
 
-            return (IQueryable<T>)query.Provider.CreateQuery(orderByCall);
+            if (terms.Length == 1)
+            {
+                return (IOrderedQueryable<T>)query.Provider.CreateQuery(orderByCall);
+            }
+            else
+            {
+                return ((IOrderedQueryable<T>)query.Provider.CreateQuery(orderByCall)).ThenBy(terms[1]);
+            }
+        }
+
+        /// <summary>
+        /// Performs further ordering given a string expression.
+        /// </summary>
+        /// <param name="query">The query to order.</param>
+        /// <param name="orderByExpression">The property (path) to order by. Append " ASC" or " DESC" to be explicit about the ordering direction.
+        /// Can contain multiple terms separated by comma, i.e. "Name, Town ASC, Country DESC".
+        /// </param>
+        /// <see href="https://stackoverflow.com/a/64085775"/>
+        public static IOrderedQueryable<T> ThenBy<T>(this IOrderedQueryable<T> query, string orderByExpression)
+        {
+            string propertyName, orderByMethod;
+            string[] terms = orderByExpression.Split(',', 2);
+            string[] strs = terms[0].Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            propertyName = strs[0];
+
+            if (strs.Length == 1)
+                orderByMethod = "ThenBy";
+            else
+                orderByMethod = strs[1].Equals("DESC", StringComparison.OrdinalIgnoreCase) ? "ThenByDescending" : "ThenBy";
+
+            ParameterExpression pe = Expression.Parameter(query.ElementType);
+            MemberExpression me = Expression.Property(pe, propertyName);
+
+            MethodCallExpression orderByCall = Expression.Call(typeof(Queryable), orderByMethod, new Type[] { query.ElementType, me.Type }, query.Expression
+                , Expression.Quote(Expression.Lambda(me, pe)));
+
+
+            if (terms.Length == 1)
+            {
+                return (IOrderedQueryable<T>)query.Provider.CreateQuery(orderByCall);
+            }
+            else
+            {
+                return ((IOrderedQueryable<T>)query.Provider.CreateQuery(orderByCall)).ThenBy(terms[1]);
+            }
         }
     }
 }
