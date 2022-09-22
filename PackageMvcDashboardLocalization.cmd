de3ad1c4ee3f90908ffb9baaa1aab62e619accc5
codetuner <codetuner@users.noreply.github.com>
2022-09-22 17:10:41 +0200

    Packaging MvcDashboardLocalization

---
+++ PackageMvcDashboardLocalization.cmd
@@ -0,0 +1,23 @@
+@ECHO OFF
+
+:: SEE ALSO: https://docs.microsoft.com/en-us/dotnet/core/tutorials/cli-templates-create-template-package
+:: SEE ALSO: https://github.com/sayedihashimi/template-sample#how-to-ship-a-template-inside-of-a-visual-studio-extension-vsix
+:: SEE ALSO: https://www.c-sharpcorner.com/article/how-to-create-a-vsix-extension-for-a-custom-template-project/
+
+XCOPY /S /Y "%~dp0MyMvcApp\Areas\MvcDashboardLocalization" "%~dp0packaging\MvcDashboardLocalization\templates\MvcDashboardLocalization\Areas\MvcDashboardLocalization\"
+XCOPY /S /Y "%~dp0MyMvcApp\Data\Localize" "%~dp0packaging\MvcDashboardLocalization\templates\MvcDashboardLocalization\Data\Localize\"
+XCOPY /S /Y "%~dp0MyMvcApp\Localize" "%~dp0packaging\MvcDashboardLocalization\templates\MvcDashboardLocalization\Localize\"
+XCOPY /S /Y "%~dp0MyMvcApp\ModelStateLocalization.json" "%~dp0packaging\MvcDashboardLocalization\templates\MvcDashboardLocalization\ModelStateLocalization.json"
+ECHO.
+ECHO Edit the template file then save and close to proceed.
+NOTEPAD "%~dp0packaging\MvcDashboardLocalization\templates\MvcDashboardLocalization\.template.config\template.json"
+ECHO Edit the project file (version nr etc) then save and close to proceed.
+NOTEPAD "%~dp0packaging\MvcDashboardLocalization\templatepack.csproj"
+PUSHD "%~dp0packaging\MvcDashboardLocalization"
+DOTNET pack
+ECHO.
+ECHO Package(s) created:
+DIR bin\Debug\*.nupkg /b /s
+ECHO.
+POPD
+PAUSE
