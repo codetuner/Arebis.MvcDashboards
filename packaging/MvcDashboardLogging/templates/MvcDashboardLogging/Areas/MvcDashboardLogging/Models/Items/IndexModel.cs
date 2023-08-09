ed74846996d21466b89f5f8c3abe7b04ac114bdf
codetuner <codetuner@users.noreply.github.com>
2023-08-09 22:21:45 +0200

    Released MvcDashboardLogging 0.9.1

--- packaging/MvcDashboardLogging/templates/MvcDashboardLogging/Areas/MvcDashboardLogging/Models/Items/IndexModel.cs
+++ packaging/MvcDashboardLogging/templates/MvcDashboardLogging/Areas/MvcDashboardLogging/Models/Items/IndexModel.cs
@@ -7,5 +7,9 @@ using System.Threading.Tasks;
 namespace MyMvcApp.Areas.MvcDashboardLogging.Models.Items
 {
     public class IndexModel : BaseIndexModel<RequestLog>
-    { }
+    { 
+        public string? AspectFilter { get; set; }
+
+        public bool Bookmarked { get; set; }
+    }
 }
