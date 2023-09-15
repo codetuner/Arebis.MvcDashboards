5d545b299b9c94261e762b6f9af250e70c9ceef7
codetuner <codetuner@users.noreply.github.com>
2023-09-16 01:12:58 +0200

    Tasks allow scheduling using local time

--- MyMvcApp/Tasks/BaseRecurrentTaskImplementation.cs
+++ MyMvcApp/Tasks/BaseRecurrentTaskImplementation.cs
@@ -18,15 +18,27 @@
         [TaskArgumentAttribute]
         public bool SkipPastSchedules { get; set; }
 
+        /// <summary>
+        /// Whether the scheduling is based on local time. By default, scheduling is based on UTC time.
+        /// I.e. when scheduling on UTC base, the exact hour of a daily task shifts with Daylight Savings Time.
+        /// When sheduling on local time, it would run at the same hour.
+        /// Note that when scheduling is based on local time, it uses the server's timzone.
+        /// </summary>
+        [TaskArgumentAttribute]
+        public bool LocalTimeBase { get; set; }
+
         public override Task Execute(ITaskHost host)
         {
             if (this.Recurrence.HasValue && this.Recurrence.Value.TotalSeconds >= 1)
             {
-                var nextTime = host.UtcTimeToExecute.Add(this.Recurrence.Value);
+                var nextTime = host.UtcTimeToExecute;
+                if (LocalTimeBase) nextTime = nextTime.ToLocalTime();
+                nextTime = nextTime.Add(this.Recurrence.Value);
                 while(SkipPastSchedules && nextTime < DateTime.UtcNow)
                 {
                     nextTime = nextTime.Add(this.Recurrence.Value);
                 }
+                if (LocalTimeBase) nextTime = nextTime.ToUniversalTime();
                 host.RescheduleCurrentTask(nextTime);
             }
 
