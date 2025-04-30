856dc56c67eada75475772e424a6c86f1336ed30
codetuner <rudi.breedenraedt@arebis.be>
2025-04-30 19:15:28 +0200

    Localize: added export to TMX.

---
+++ MyMvcApp/Areas/MvcDashboardLocalize/Models/Domain/ExportToTmxModel.cs
@@ -0,0 +1,11 @@
+﻿namespace MyMvcApp.Areas.MvcDashboardLocalize.Models.Domain
+{
+    public class ExportToTmxModel
+    {
+        public int DomainId { get; set; }
+
+        public bool IncludeNonReviewed { get; set; }
+        
+        public bool IncludeNotes { get; set; }
+    }
+}
